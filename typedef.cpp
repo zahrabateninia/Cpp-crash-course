decfcd5381573f97ceae2a56a672073705aee013
zahrabateninia <zahrabatenin@gmail.com>
2024-10-12 19:33:38 +0330

    Use using keyword instead of typedef

--- typedef.cpp
+++ typedef.cpp
@@ -3,9 +3,13 @@
 
 int main(){
     // typedef std::vector<std::pair<std::string, int >> pairlist_t; // pairlist_t is the identifier for this datatype. Much more readable!
-    typedef std::string text_t;
-    typedef int number_t;
-    
+    // typedef std::string text_t;
+    // typedef int number_t;
+
+    //  using new_identifier = datatype
+    using text_t = std::string;
+    using number_t = int;
+
     text_t name = "Zahra Bateninia";
     number_t age = 23;
 
