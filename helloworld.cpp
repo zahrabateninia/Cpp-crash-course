a278483712096f178b8a31ad85df7e237a8817e9
zahrabateninia <zahrabatenin@gmail.com>
2024-10-12 17:17:17 +0330

    Declare and assign variables.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,11 @@
 #include <iostream> // iostream is a header file that contains functions for basic input output operations
 
 int main(){
-    std::cout<<"Hello World!"; //std::cout means standard character output, << is left shift operator
+    int age = 23;
+    std::string name = "Zahra";
 
+    std::cout<<"Hello, "<<name<<'\n'; //std::cout means standard character output, << is left shift operator when used with numbers
+    std::cout<<"How are you?"<<std::endl; // instead of <<std::endl use '\n'
+    std::cout<<"You are "<<age<<" years old."<<'\n';
     return 0;
 }
