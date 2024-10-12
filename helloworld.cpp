6b5b596e853528e7268153476efd84ea760fa41a
zahrabateninia <zahrabatenin@gmail.com>
2024-10-12 16:52:16 +0330

    Print Hello World! in the terminal

---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // iostream is a header file that contains functions for basic input output operations
+
+int main(){
+    std::cout<<"Hello World!"; //std::cout means standard character output, << is left shift operator
+
+    return 0;
+}
